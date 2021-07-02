5a2e049a113c0d5d6795b0e5733eb27f3b18d150
muadhab <mouadh.aboud@gmail.com>
2021-07-03 00:52:03 +0200

    4.0
    4.0

---
+++ AndroidProject/app/src/main/java/com/example/androidproject/page1.java
@@ -0,0 +1,17 @@
+package com.example.androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidproject.R;
+
+
+public class page1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page1);
+    }
+}
